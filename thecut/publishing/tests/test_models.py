e4750229c553c8056af82a799d37842da7567781
Josh Crompton <josh.crompton@thecut.net.au>
2014-07-07 14:27:45 +0800

    Update project with testing infrastructure using tox.

---
+++ thecut/publishing/tests/test_models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_news
+------------
+
+Tests for `news` models module.
+"""
+
+import os
+import shutil
+import unittest
+
+#from news import models
+
+
+class TestNews(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        self.assertTrue(False)
+
+    def tearDown(self):
+        pass
