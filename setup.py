2b775eea0a0dfc674f58dcbd14e47bf166dfec96
Matt Austin <matt.austin@thecut.net.au>
2014-11-21 16:29:36 +0800

    Keep PassThroughManager for compatibility with pre 1.7 django versions. Revert "Removed model utils requirement, and use of PassThroughManager (can now use .as_manager() on querysets in Django 1.7)."
    This reverts commit 340dc7d984086912c625da1474d0de1e907d4016.
    
    Conflicts:
    	setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     version=get_git_version(),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['django-taggit==0.12.2'],
+    install_requires=['django-model-utils>=2.2.0,<3', 'django-taggit==0.12.2'],
 )
