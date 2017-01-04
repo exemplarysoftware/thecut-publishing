2e7e1cfc7e4f1aabbe39895c8812215711fc244b
Mark Lockett <mark.lockett@thecut.net.au>
2017-01-04 09:28:34 +0800

    Get unittests to run in tox Res 9326

--- runtests.py
+++ runtests.py
@@ -17,6 +17,7 @@ try:
             "django.contrib.auth",
             "django.contrib.contenttypes",
             "django.contrib.sites",
+            'taggit',
             "thecut.publishing",
             "test_app",
         ],
