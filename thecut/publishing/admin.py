276630e238871337fbdb70693dd0c1c9b1064098
Matt Austin <matt.austin@thecut.net.au>
2017-05-10 10:46:55 +0800

    Use consistent ``coding`` declaration.

--- thecut/publishing/admin.py
+++ thecut/publishing/admin.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.contrib import admin, messages
 from django.core.exceptions import FieldDoesNotExist
